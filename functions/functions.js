c1c29f5f16b1b15fc30a685b53a4420d3e984a90
Amber Brasher <abrasher@urbantech.com>
2022-06-16 17:00:48 -0400

    pls work

---
+++ functions/functions.js
@@ -0,0 +1,11 @@
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number.
+ * @param second Second number.
+ * @returns The sum of the two numbers.
+ */
+
+function add(first, second) {
+  return first + second
+}
